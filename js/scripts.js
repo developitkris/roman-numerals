0f0f4e0b261fec849b3dbb45f1f55d50273a8db9
CJ Rhone and Kristi Hwang <kristi@>
2018-03-29 10:53:48 -0700

    Add part business logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,36 @@ $(document).ready(function() {
   $("form#romanForm").submit(function(event) {
     event.preventDefault();
 
-    $(".result").show();
+    var userNum = parseInt($("#numberInput").val());
+    var finalResult =  romanize(userNum);
+    alert(finalResult);
+
+    $("#result").text(finalResult);
 
 
 
   });
 });
-var userNum = $("#numberInput").val();
+
 
 
 
 
 //Business Logic
 var roman= ["M", "D", "C", "L", "X", "V", "I"];
-var numbers=[1000, 500, 100, 50, 10, 5, 1];
+var number=[1000, 500, 100, 50, 10, 5, 1];
+var output = "";
+
+function romanize(integer){
+  console.log("Im in here!")
+  for(var i=0; i<integer; i++){
+    console.log("after for loop");
+    if (integer >= number[i]){
+      output+=roman[i];
+      integer-= number[i];
+
+    };
+
+  };
+return output;
+};
